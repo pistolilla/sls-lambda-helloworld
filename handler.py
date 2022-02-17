ed980db6ab3c288eaa0140e8c04ce5564c3df6f9
Leopoldo Varela <leopoldo.varela@everstream.ai>
2022-02-17 17:52:45 -0600

    adding post endpoint sample

--- handler.py
+++ handler.py
@@ -1,16 +1,24 @@
 import json
 import datetime
 
+def response(body:dict, code:int=200):
+    return {
+        "statusCode": code,
+        "headers": {
+            "content-type": "application/json",
+        },
+        "body": json.dumps(body)
+    }
 
 def endpoint(event, context):
+    """Returns a timestamp"""
     current_time = datetime.datetime.now().time()
     body = {
         "message": "Hello, the current time is " + str(current_time)
     }
+    return response(body)
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
+def hello(event, context):
+    """Returns the variables found in the request body"""
+    data = json.loads(event["body"])
+    return response(data)
